a1f6bbc2ba0c1bd5750773de93699af2de779898
smomin156 <63006524+smomin156@users.noreply.github.com>
2021-09-02 01:12:58 +0530

    Add files via upload

---
+++ DataGridCalculativeColumn/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGridCalculativeColumn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NorthwindEntities db = null;
+        ObservableCollection<Product> productCollection;
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new NorthwindEntities();
+        }
+
+        private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            productCollection = new ObservableCollection<Product>(db.Products.OrderByDescending(p => p.ProductID));
+            datagrid1.DataContext = productCollection;
+        }
+    }
+}
---
+++ DataGridCalculativeColumn/NetValueCalculation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DataGridCalculativeColumn
+{
+    //public class NetValueCalculation : IMultiValueConverter
+    //{
+    //    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        if (values != null)
+    //        {
+
+    //            decimal NetStock = (short)values[0] * (short)values[1];
+    //            return NetStock.ToString();
+    //        }
+    //        return "";
+    //    }
+
+    //    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+    public class NetValueCalculation : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Product product = value as Product;
+            decimal? NetStock = product.UnitPrice * product.UnitsInStock;
+            return NetStock.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
